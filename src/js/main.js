be4e73e59774193984a86e55be053e54cd93bf84
eliasjonah <ilyas_19.04.20@mail.ru>
2023-01-26 19:36:43 +0200

    scroll is done

--- src/js/main.js
+++ src/js/main.js
@@ -10,5 +10,5 @@ window.addEventListener('DOMContentLoaded', () => {
 
 // в load следует добавить скрипты, не участвующие в работе первого экрана
 window.addEventListener('load', () => {
-  // ticker();
+  ticker();
 });
--- src/js/modules/ticker.js
+++ src/js/modules/ticker.js
@@ -2,22 +2,73 @@ const ticker = () => {
   const tickerLeft = document.querySelectorAll('.ticker-left');
   const tickerRight = document.querySelector('.ticker-right');
   const cursor = document.querySelector('.cursor');
+  const wrapper = document.querySelector('.ticker__wrapper');
+  const ticker = document.querySelector('.ticker__item');
+  let list2;
 
-  // Scroll
-  window.onscroll = () => {
-    let pos = window.scrollY - 800;
-    tickerRight.style.right = `${pos}px`;
+  const speed = 1;
 
-    tickerLeft.forEach((item) => {
-      item.style.left = `${pos}px`;
-    })
-  };
+  const width = ticker.offsetWidth;
+  let x = 0;
+  let x2 = width;
 
-  // Cursor
+  function clone() {
+    list2 = ticker.cloneNode(true);
+    wrapper.appendChild(list2);
+    list2.style.transform = `translateX(${width}px)`;
+  }
+
+
+  function moveFirst() {
+    x -= speed;
+
+    if (width >= Math.abs(x)) {
+      ticker.style.transform = `translateX(${x}px)`;
+    } else {
+      x = width;
+    }
+  }
+
+  function moveSecond() {
+    x2 -= speed;
+
+    if (list2.offsetWidth >= Math.abs(x2)) {
+      list2.style.transform = `translateX(${x2}px)`;
+    } else {
+      x2 = width;
+    }
+  }
+
+  // ss
+
+  // function scroll() {
+  //   window.addEventListener("scroll", () => {
+  //     clearInterval(a);
+  //     clearInterval(b);
+  //     let pos = window.scrollY - 100;
+  //     ticker.style.left = `${pos}px`;
+  //     list2.style.left = `${pos}px`;
+  //   });
+    // window.onscroll = () => {
+    //   clearInterval(a);
+    //   clearInterval(b);
+    //   let pos = window.scrollY - 100;
+    //   ticker.style.left = `${pos}px`;
+    //   list2.style.left = `${pos}px`;
+    // };
+  // }
+
+
   document.addEventListener('mousemove', (e) => {
     cursor.style.left = e.clientX + 'px';
     cursor.style.top = e.clientY + 'px';
   });
+
+  clone();
+  let a = setInterval(moveFirst, 10);
+  let b = setInterval(moveSecond, 10);
+  // scroll();
 };
+
 
 export {ticker};
